2c25d8f7af57e597941a322bd5fd776da0aa5794
pt <therobotprogrammer@gmail.com>
2019-04-06 21:16:09 +0530

    example plots - added title information and fixed bugs

--- my variation - regression-toy example.py
+++ my variation - regression-toy example.py
@@ -42,7 +42,7 @@ def generate_data(n_samples, n_features):
         y = 0.5 + 1.5 * X[:,0]**2 + 1.6 * X[:,1]**3
     return X, y
 
-def plot_data_2d(X, y, limit_y=None):
+def plot_data_2d(X, y, limit_y=None, message = ''):
     plt.figure()
     labels =['X'+str(i) for i in range(X.shape[1])]
     if limit_y:
@@ -50,9 +50,10 @@ def plot_data_2d(X, y, limit_y=None):
     plt.scatter(X, y, c='blue')
     plt.xlabel(labels[0])
     plt.ylabel("target")
+    plt.title(message, fontdict = {'fontsize' : 20})
     plt.show()
 
-def plot_model_2d(estimator, X, y, limit_y=None):
+def plot_model_2d(estimator, X, y, limit_y=None, message = ''):
     plt.figure()
     labels =['X'+str(i) for i in range(X.shape[1])]
 
@@ -63,9 +64,10 @@ def plot_model_2d(estimator, X, y, limit_y=None):
     if limit_y:
         plt.ylim(limit_y[0], limit_y[1])
     plt.plot(xx, y_pred, color='red')
+    plt.title(message, fontdict = {'fontsize' : 20})
     plt.show()
 
-def plot_data_3d(X, y):
+def plot_data_3d(X, y, message = ''):
     labels =['X'+str(i) for i in range(X.shape[1])]
     fig = plt.figure()
     ax = plt.axes(projection='3d')    
@@ -73,13 +75,15 @@ def plot_data_3d(X, y):
     ax.set_xlabel(labels[0])
     ax.set_ylabel(labels[1])
     ax.set_zlabel("target")
+    plt.title(message, fontdict = {'fontsize' : 20})
+    
 #    for angle in range(0, 360):
 #        ax.view_init(30, angle)
 #        plt.draw()
 #        plt.pause(.01)
     plt.show()
 
-def plot_model_3d(estimator, X, y):
+def plot_model_3d(estimator, X, y, message = ''):
     labels =['X'+str(i) for i in range(X.shape[1])]
 
     x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
@@ -96,6 +100,7 @@ def plot_model_3d(estimator, X, y):
     ax.set_xlabel(labels[0])
     ax.set_ylabel(labels[1])
     ax.set_zlabel("target")
+    plt.title(message, fontdict = {'fontsize' : 20})
     plt.tight_layout()    
     
 #    for angle in range(0, 360):
@@ -150,7 +155,7 @@ def get_model_tree(estimator, grid, X, y, path="C://tree.pdf"):
     return final_model
 
 def get_model_ensemble(estimator, grid, X, y):
-    grid_estimator = model_selection.GridSearchCV(estimator, grid, scoring=metrics.make_scorer(rmse, greater_is_better=False), cv=10)
+    grid_estimator = model_selection.GridSearchCV(estimator, grid, scoring=metrics.make_scorer(rmse, greater_is_better=False), cv=10, n_jobs = -1)
     grid_estimator.fit(X, y)
     print(grid_estimator.best_params_)
     final_model = grid_estimator.best_estimator_
@@ -158,7 +163,7 @@ def get_model_ensemble(estimator, grid, X, y):
     print(grid_estimator.score(X, y))
     return final_model
 
-def grid_search_one_parameter(estimator, grid, X, y, scoring="accuracy"):
+def grid_search_one_parameter(estimator, grid, X, y, scoring="accuracy", message = ''):
     plt.figure()
     grid_estimator = model_selection.GridSearchCV(estimator, grid, cv=10, return_train_score=True, scoring = scoring, n_jobs = -1)
     grid_estimator.fit(X, y)
@@ -186,9 +191,10 @@ def grid_search_one_parameter(estimator, grid, X, y, scoring="accuracy"):
     plt.ylabel("Performance")
     plt.tight_layout()
     plt.legend(loc="best")
+    plt.title('Grid Search: ' + message, fontdict = {'fontsize' : 20})
     plt.show()
     
-def grid_search_two_parameters(estimator, grid, X, y, scoring="accuracy"):
+def grid_search_two_parameters(estimator, grid, X, y, scoring="accuracy", message = ''):
     grid_estimator = model_selection.GridSearchCV(estimator, grid, cv=10, scoring = scoring, return_train_score=True, n_jobs = -1)
     grid_estimator.fit(X, y)
 
@@ -197,7 +203,7 @@ def grid_search_two_parameters(estimator, grid, X, y, scoring="accuracy"):
     param2_name = list(grid.keys())[1]
     param2_range = grid.get(param2_name)
         
-    fig = plt.figure()
+    plt.figure()
     ax = plt.axes(projection='3d')
     X, Y = np.meshgrid(param1_range, param2_range)
     
@@ -210,112 +216,150 @@ def grid_search_two_parameters(estimator, grid, X, y, scoring="accuracy"):
     ax.set_xlabel(param1_name)
     ax.set_ylabel(param2_name)
     ax.set_zlabel('performance')
-    #plt.legend(loc="best")
+#    plt.legend(loc="best")
     plt.tight_layout()
-
-#    for angle in range(0, 360):
+    plt.title('Grid Search: ' + message, fontdict = {'fontsize' : 20})
+    
+   
+#    for angle in range(0, 180):
 #        ax.view_init(20, angle)
 #        plt.draw()
 #        plt.pause(.01)
     plt.show()
 
+
 #linear pattern in 2d
+message = 'Linear Pattern in 2d'
 X, y = make_regression(n_samples=200, n_features=1, n_informative=1, random_state=0, noise=20, bias = 250)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=1)
-plot_data_2d(X_train, y_train)
+plot_data_2d(X_train, y_train, message = message)
 
+
+
+message = 'Linear Regression'
 linear_estimator = linear_model.LinearRegression()
 linear_grid = {'normalize': [True, False]}
 final_estimator = get_model_objective(linear_estimator, linear_grid, X_train, y_train)
-plot_model_2d(final_estimator, X_train, y_train)
+plot_model_2d(final_estimator, X_train, y_train, message = message)
+
+
 
 #linear pattern in 3d
+message = 'Data - linear pattern in 3d'
 X, y = make_regression(n_samples=200, n_features=2, n_informative=2, random_state=0, noise=0, bias = 250)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=1)
-plot_data_3d(X_train, y_train)
+plot_data_3d(X_train, y_train, message = message)
 
 
+
+message = 'Linear Regression'
 linear_estimator = linear_model.LinearRegression()
 linear_grid = {'normalize': [True, False]}
 final_estimator = get_model_objective(linear_estimator, linear_grid, X_train, y_train)
-plot_model_3d(final_estimator, X_train, y_train)
+plot_model_3d(final_estimator, X_train, y_train, message = message)
 
 
 #non-linear pattern in 2d
+message = 'Data - Non-Linear pattern in 2d'
 X, y = generate_data(n_samples=100, n_features=1)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=1)
-plot_data_2d(X_train, y_train)
+plot_data_2d(X_train, y_train, message = message)
 
 
+message = 'Linear Regression 2D'
 linear_estimator = linear_model.LinearRegression()
 linear_grid = {'normalize': [True, False]}
 final_estimator = get_model_objective(linear_estimator, linear_grid, X_train, y_train)
-plot_model_2d(final_estimator, X_train, y_train)
+plot_model_2d(final_estimator, X_train, y_train, message = message)
 
 
+message = 'Poly Estimator 2D'
 poly_estimator = Pipeline([('features', PolynomialFeatures()) ,
                           ('estimator', linear_model.LinearRegression())]
                           )
 poly_grid = {'features__degree':list(range(1,15))}
 final_estimator = get_model_objective(poly_estimator, poly_grid, X_train, y_train)
-grid_search_one_parameter(poly_estimator, poly_grid, X_train, y_train, scoring = metrics.make_scorer(rmse))
-plot_model_2d(final_estimator, X_train, y_train)
+grid_search_one_parameter(poly_estimator, poly_grid, X_train, y_train, scoring = metrics.make_scorer(rmse), message = message)
+plot_model_2d(final_estimator, X_train, y_train, message = message)
 
+
+message = 'Decision Tree Regressor'
 dt_estimator = tree.DecisionTreeRegressor()
 dt_grid = {'max_depth':list(range(1,10))}
 final_estimator = get_model_tree(dt_estimator, dt_grid, X_train, y_train)
 grid_search_one_parameter(dt_estimator, dt_grid, X_train, y_train, scoring = metrics.make_scorer(rmse))
-plot_model_2d(final_estimator, X_train, y_train)
+plot_model_2d(final_estimator, X_train, y_train, message = message)
 
+
+message = 'Random Forest Regressor'
 rf_estimator = ensemble.RandomForestRegressor()
 rf_grid = {'n_estimators':list(range(1,100,20)), 'max_depth':list(range(1,5))}
 final_estimator = get_model_ensemble(rf_estimator, rf_grid, X_train, y_train)
-grid_search_two_parameters(rf_estimator, rf_grid, X_train, y_train, scoring = metrics.make_scorer(rmse))
-plot_model_2d(final_estimator, X_train, y_train)
+grid_search_two_parameters(rf_estimator, rf_grid, X_train, y_train, scoring = metrics.make_scorer(rmse), message = message)
+plot_model_2d(final_estimator, X_train, y_train, message = message)
 
+
+
+message = 'Linear SVM'
 svr_lin = svm.SVR(kernel='linear')
 svr_lin_grid = {'C':[20, 50, 100] }
 final_estimator = get_model_objective(svr_lin, svr_lin_grid, X_train, y_train)
 grid_search_one_parameter(svr_lin, svr_lin_grid, X_train, y_train, scoring = metrics.make_scorer(rmse))
-plot_model_2d(final_estimator, X_train, y_train)
+plot_model_2d(final_estimator, X_train, y_train, message = message)
 
+
+message = 'Kernel SVR (RBF)'
 svr_rbf = svm.SVR(kernel='rbf')
 svr_rbf_grid = {'C':[50, 100], 'gamma':[0.1, 0.2] }
 final_estimator = get_model_objective(svr_rbf, svr_rbf_grid, X_train, y_train)
-grid_search_two_parameters(svr_rbf, svr_rbf_grid, X_train, y_train, scoring = metrics.make_scorer(rmse))
-plot_model_2d(final_estimator, X_train, y_train)
+grid_search_two_parameters(svr_rbf, svr_rbf_grid, X_train, y_train, scoring = metrics.make_scorer(rmse), message = message)
+plot_model_2d(final_estimator, X_train, y_train, message = message)
 
+
+message = 'Kernel SVR (Poly)'
 svr_poly = svm.SVR(kernel='poly')
 svr_poly_grid = {'C':[50, 100], 'degree':[2, 3] }
 final_estimator = get_model_objective(svr_poly, svr_poly_grid, X_train, y_train)
-grid_search_two_parameters(svr_poly, svr_poly_grid, X_train, y_train, scoring = metrics.make_scorer(rmse))
-plot_model_2d(final_estimator, X_train, y_train)
+grid_search_two_parameters(svr_poly, svr_poly_grid, X_train, y_train, scoring = metrics.make_scorer(rmse), message = message)
+plot_model_2d(final_estimator, X_train, y_train, message = message)
 
+
+message = 'KNN Regressor'
 knn_estimator = neighbors.KNeighborsRegressor()
 knn_grid = {'n_neighbors':[8] }
 final_estimator = get_model_neighbors(knn_estimator,knn_grid, X_train, y_train)
-grid_search_one_parameter(knn_estimator, knn_grid, X_train, y_train, scoring = metrics.make_scorer(rmse))
-plot_model_2d(final_estimator, X_train, y_train)
+grid_search_one_parameter(knn_estimator, knn_grid, X_train, y_train, scoring = metrics.make_scorer(rmse), message = message)
+plot_model_2d(final_estimator, X_train, y_train, message = message)
+
+
 
 #non-linear pattern in 3d
+message = 'Non-Linear Pattern in 3d'
 X, y = generate_data(n_samples=100, n_features=2)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=1)
-plot_data_3d(X_train, y_train)
+plot_data_3d(X_train, y_train, message = message)
 
+
+
+message = 'Linear Regression in 3d'
 linear_estimator = linear_model.LinearRegression()
 linear_grid = {'normalize': [True, False]}
 final_estimator = get_model_objective(linear_estimator, linear_grid, X_train, y_train)
-plot_model_3d(final_estimator, X_train, y_train)
+plot_model_3d(final_estimator, X_train, y_train, message = message)
 
+
+message = 'KNN Regression in 3d'
 knn_estimator = neighbors.KNeighborsRegressor()
 knn_grid = {'n_neighbors':list(range(1,10)) }
 final_estimator = get_model_neighbors(knn_estimator,knn_grid, X_train, y_train)
-grid_search_one_parameter(knn_estimator, knn_grid, X_train, y_train, scoring = metrics.make_scorer(rmse))
-plot_model_3d(final_estimator, X_train, y_train)
+grid_search_one_parameter(knn_estimator, knn_grid, X_train, y_train, scoring = metrics.make_scorer(rmse), message = message)
+plot_model_3d(final_estimator, X_train, y_train, message = message)
 
+
+message = 'SVM Kernel (RBF) 3d'
 svr_rbf = svm.SVR(kernel='rbf')
 svr_rbf_grid = {'C':[50, 100], 'gamma':[0.1, 0.2] }
 final_estimator = get_model_objective(svr_rbf, svr_rbf_grid, X_train, y_train)
-grid_search_two_parameters(svr_rbf, svr_rbf_grid, X_train, y_train, scoring = metrics.make_scorer(rmse))
-plot_model_3d(final_estimator, X_train, y_train)
+grid_search_two_parameters(svr_rbf, svr_rbf_grid, X_train, y_train, scoring = metrics.make_scorer(rmse), message = message)
+plot_model_3d(final_estimator, X_train, y_train, message = message)
 
